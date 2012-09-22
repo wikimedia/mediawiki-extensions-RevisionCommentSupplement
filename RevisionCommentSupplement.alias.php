386e3dea0d3039533ad7888a99437cfd51f4966b
Raimond Spekking <raimond.spekking@gmail.com>
2012-09-22 07:41:02 +0000

    Revert "Localisation updates from http://translatewiki.net."
    TWN export script broke files due to some style error in the files.
    
    This reverts commit 0bf3921a1dbf69e9d72e0affcbe9c55c8aea51af

--- RevisionCommentSupplement.alias.php
+++ RevisionCommentSupplement.alias.php
@@ -1,13 +1,11 @@
-<?
+<?php
 
 $specialPageAliases = array();
 
-/** English (English) */
 $specialPageAliases['en'] = array(
 	'RevisionCommentSupplement' => array( 'RevisionCommentSupplement', 'Revision Comment Supplement' ),
 );
 
-/** Japanese (日本語) */
 $specialPageAliases['ja'] = array(
 	'RevisionCommentSupplement' => array( '版のコメントの補足' ),
 );
